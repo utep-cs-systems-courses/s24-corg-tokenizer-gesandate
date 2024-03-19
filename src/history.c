b7bf5f390db89204f76c606750ee11a424875bae
gesandate <gsandate2@miners.utep.edu>
2024-03-19 17:44:03 -0600

    Started the history file

---
+++ src/history.c
@@ -0,0 +1,27 @@
+#include "stdio.h"
+#include "history.h"
+#include "stdlib.h"
+
+List* init_history(){
+  List* ptr = (List*)malloc(sizeof(List));
+  ptr -> root= NULL;
+  return ptr;
+}
+
+void add_history(List *list, char *str){
+  (list->root == NULL) ? root -> str: s_item -> str;
+}
+
+char *get_history(List *list, int id){
+
+}
+
+void print_history(List *list){
+
+}
+
+void free_history(List *list){
+
+}
+
+int main(){}
